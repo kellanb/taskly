3213ea0dbe86e922748a8778426cf8ff45165cbe
Kellan Benn <kellanbenn@gmail.com>
2025-05-08 15:55:45 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good.",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleDelete}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +39,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
